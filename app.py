4aa64fb9cfa1c43f3246e12a67893505e797f141
Einenlum <yann.rabiller@gmail.com>
2021-03-02 01:28:01 +0100

    Use a wrapper to create wsgi app

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from view import hello_pycon
 from http_utils import Request
 
 
-def my_application(environ, start_response):
-    request = Request.build_from_wsgi(environ)
+def wsgi_app(function):
+    def wrapper(environ, start_response):
+        request = Request.build_from_wsgi(environ)
+        response = function(request)
+        start_response(response.status, response.headers)
 
-    response = hello_pycon(request)
+        return iter([response.content])
 
-    start_response(response.status, response.headers)
+    return wrapper
 
-    return iter([response.content])
+@wsgi_app
+def my_app(request):
+    return hello_pycon(request)
